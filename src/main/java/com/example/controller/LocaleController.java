2df39b8905ef4cd79b614df9faa4d021c286e2b8
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-10-03 11:34:58 +0530

    committed

---
+++ src/main/java/com/example/controller/LocaleController.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LocaleController {
+    @RequestMapping("/locale")
+    public String locale() {
+        return "locale";
+    }
+}
